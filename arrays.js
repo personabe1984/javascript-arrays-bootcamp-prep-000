1c57bbcebcd24fd9fe5231092041fa5764ea6267
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-18 02:02:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var nArray = [element, ...array];
+  return nArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var nArray = [...array, element];
+  return nArray;
+}
+
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
