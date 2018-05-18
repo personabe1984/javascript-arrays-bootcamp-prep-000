4e27abcf49781281cb463627db1ac30d5bfa2f75
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-18 02:03:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,6 @@ function addElementToEndOfArray(array, element){
 
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
+  return array.push(element);
+;
 }
